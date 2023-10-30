bba1b124ae6dfc30bc640aad0ccdc32b5eddfe18
Juan Manuel Lopez <juanmlopez02@gmail.com>
2023-10-30 13:56:43 -0300

    AICL-3 AICL-4: additional corrections discussed

--- api/query.py
+++ api/query.py
@@ -6,7 +6,7 @@ from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from utils.process import write_code_from_response
+from utils.process import extract_code_from_response
 
 os.environ["OPENAI_API_KEY"] = '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'
 
@@ -34,8 +34,9 @@ def get_suggestion_for_text(obj, info, text):
         "errors": []
     }
 
+
 @convert_kwargs_to_snake_case
-def get_answer_for_question(obj, info, question, create_file, directory_path, filename="archivo"):
+def get_answer_for_question(obj, info, question):
     persist_directory = 'db_code'
     embedding = OpenAIEmbeddings()
 
@@ -51,15 +52,15 @@ def get_answer_for_question(obj, info, question, create_file, directory_path, fi
 
     result = qa({"question": question, "chat_history": chat_history})
 
-    if create_file:
-        code_result = write_code_from_response(result['answer'], directory_path, filename)
-        if code_result == 1:
-            return {
-                "result": f"File '{filename}' successfully created in '{directory_path}'",
-                "errors": []
-            }
+    code_blocks = extract_code_from_response(result['answer'])
 
-    return {
+    if code_blocks.__len__() > 0:
+        return {
+            "result": code_blocks[0][1],
+            "errors": []
+        }
+    else:
+        return {
             "result": result['answer'],
             "errors": []
         }
--- utils/process.py
+++ utils/process.py
@@ -3,8 +3,15 @@ from bs4 import BeautifulSoup
 import re
 import os
 
+language_extensions = {
+    'python': 'py',
+    'javascript': 'js',
+    'java': 'java',
+}
+
+
 def write_code_from_response(response, directory, filename):
-    code_blocks = re.findall(r'```(python|javascript|java)(.*?)```', response, re.DOTALL)
+    code_blocks = extract_code_from_response(response)
 
     if code_blocks.__len__() > 0:
         language = code_blocks[0][0]
@@ -12,18 +19,14 @@ def write_code_from_response(response, directory, filename):
 
         if code is not None:
             extension = ".txt"
-            match language:
-                case "python":
-                    extension = ".py"
-                case "java":
-                    extension = ".java"
-                case "javascript":
-                    extension = ".js"
+            for lang, ext in language_extensions.items():
+                if lang == language:
+                    extension = '.' + ext
+                    break
 
             filename += extension
-            create_and_write_code_file(directory, filename, code)
-        return 1
-
+            result = create_and_write_code_file(directory, filename, code)
+            return result
     else:
         return 0
 
@@ -34,6 +37,15 @@ def create_and_write_code_file(directory, filename, code):
     try:
         with open(file_path, 'w') as file:
             file.write(code)
+            return 1
 
     except Exception as e:
         print(f"Error: {e}")
+        return 0
+
+
+def extract_code_from_response(response):
+    regex_pattern = r'```(' + '|'.join(f'{language}' for language, ext in language_extensions.items()) + r')(.*?)```'
+    code_blocks = re.findall(regex_pattern, response, re.DOTALL)
+
+    return code_blocks
